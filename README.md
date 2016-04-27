f4918a9fe93f835e31d64584c9ea55abc7d96073
gabo <deobieta@gmail.com>
2016-04-26 20:04:58 -0500

    more ansible-playbook options and copy EC2 dynamic inventory and ini

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # docker-ansible
+
+Run ansible playbooks with different versions. Useful to test ansbile 2
+
+## USAGE
+
+The following environment variables are honored for configuring ansible:
+
+-	`-e ANSIBLE_CFG_HOST_KEY_CHECKING=...` (Set to enable host key checking. Default False)
+-	`-e ANSIBLE_CFG_PRIVATE_KEY=...` (Path to private key. Default /root/.ssh/id_rsa)
+-	`-e ANSIBLE_EC2=...` (Enable EC2 Dynamic inventory and options)
+-	`-e AWS_ACCESS_KEY_ID=...` (AWS access key. Mandatory when ANSIBLE_EC2 enabled)
+-	`-e AWS_SECRET_ACCESS_KEY=...` (AWS secret key. Mandatory when ANSIBLE_EC2 enabled)
+-	`-e ANSIBLE_EC2_INI_REGIONS=...` (Set regions to query. Default all)
+-	`-e ANSIBLE_EC2_INI_ALL_INSTANCES=...` (Query all instances despite their status)
+-	`-e ANSIBLE_EC2_INI_CACHE_MAX_AGE=...` (Set cache max age. Default 300) Doesn't make any sense if you create the container every run
+-	`-e ANSIBLE_EC2_INI_INSIDE_VPC=...` (Enable if you are running your playbook inside the VPC)
+-	`-e ANSIBLE_EC2_INCLUDE_RDS=...` (Enable to include RDS instances)
+-	`-e ANSIBLE_EC2_INCLUDE_ELASTICACHE=...` (Enable to include ELASTICACHE instances)
+-	`-e REQUIREMENTS=...` (Install dependencies if file exists)
+-	`-e PLAYBOOK=...` (Name of your playbook. Default playbook.yml)
+-	`-e INVENTORY=...` (Define inventory file. Default /etc/ansible/hosts)
+-	`-e CONNECTION_LOCAL=...` (Set to append --connection=local)
+-	`-e INVENTORY_HOST=...` (Append host to /etc/ansible/hosts)
+
+Quickstart:  
+
+    $ docker run -ti --rm \
+        --name=ansible202 \
+        -e "ANSIBLE_EC2=true" \
+        -e "AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" \
+        -e "AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXX" \
+        -e "ANSIBLE_EC2_INI_REGIONS=us-east-1,us-west-1,us-west-2" \
+        -e "REQUIREMENTS=/etc/ansible/roles/requirements.yml" \
+        -e "PLAYBOOK=/etc/ansible/roles/devel.yml" \
+        --volume=$(pwd):/etc/ansible/roles \
+        --volume private_key:/root/.ssh/id_rsa \
+        aguamala/ansible:2.0.2
+
+    $ docker run -ti --rm \
+        --name=ansible202 \
+        -e "ANSIBLE_EC2=true" \
+        -e "AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" \
+        -e "AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXX" \
+        -e "ANSIBLE_EC2_INI_REGIONS=us-east-1,us-west-1,us-west-2" \
+        -e "REQUIREMENTS=/etc/ansible/roles/requirements.yml" \
+        -e "PLAYBOOK=/etc/ansible/roles/devel.yml" \
+        --volume=$(pwd):/etc/ansible/roles \
+        --volume private_key:/root/.ssh/id_rsa \
+        aguamala/ansible:1.9.4
--- build-194/docker-entrypoint.sh
+++ build-194/docker-entrypoint.sh
@@ -4,6 +4,8 @@
 set -e
 
 if [ "$1" = 'ansible-playbook' ]; then
+
+    #ANSIBLE_CFG OPTIONS
     if [ -z "$ANSIBLE_CFG_HOST_KEY_CHECKING" ]; then
         sed -i -e"s|#host_key_checking = False|host_key_checking = False|g" /etc/ansible/ansible.cfg
     fi
@@ -14,9 +16,16 @@ if [ "$1" = 'ansible-playbook' ]; then
         sed -i -e"s|#private_key_file = /path/to/file|private_key_file = /root/.ssh/id_rsa|g" /etc/ansible/ansible.cfg
     fi
 
+    #ANSIBLE_EC2 OPTIONS
     if [ -n "$ANSIBLE_EC2" ]; then
 
-        ENV INVENTORY /etc/ansible/ec2.py
+        if [[ -z "$AWS_ACCESS_KEY_ID" ]] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+            echo >&2 'error: missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY environment variables'
+            exit 1
+        fi
+
+        #FORCE DYNAMIC INVENTORY
+        INVENTORY=/etc/ansible/ec2.py
 
         if [ -n "$ANSIBLE_EC2_INI_REGIONS" ]; then
             sed -i -e"s|regions = all|regions = $ANSIBLE_EC2_INI_REGIONS|g" /etc/ansible/ec2.ini
@@ -34,26 +43,46 @@ if [ "$1" = 'ansible-playbook' ]; then
             sed -i -e"s|destination_variable = public_dns_name|destination_variable = private_dns_name|g" /etc/ansible/ec2.ini
             sed -i -e"s|vpc_destination_variable = ip_address|vpc_destination_variable = private_ip_address|g" /etc/ansible/ec2.ini
         fi
+
+        if [ -z "$ANSIBLE_EC2_INCLUDE_RDS" ]; then
+            sed -i -e"s|#rds = False|rds = False|g" /etc/ansible/ec2.ini
+        fi
+
+        if [ -z "$ANSIBLE_EC2_INCLUDE_ELASTICACHE" ]; then
+            sed -i -e"s|#elasticache = False|elasticache = False|g" /etc/ansible/ec2.ini
+        fi
+
     fi
 
+    #INSTALL PLAYBOOK REQUIREMENTS
     if [ -f "$REQUIREMENTS" ]; then
-        ansible-galaxy install -r $REQUIREMENTS
+        ansible-galaxy install -f -r $REQUIREMENTS
     fi
 
-    #if [ -z "$PLAYBOOK" ]; then
-    #    PLAYBOOK=playbook.yml
-    #fi
-
-    if [ -n "$CONNECTION_LOCAL" ]; then
-        exec "$@" --connection=local
+    #DEFAULT PLAYBOOK NAME
+    if [ -z "$PLAYBOOK" ]; then
+        PLAYBOOK=playbook.yml
     fi
 
-    if [ -z "$INVENTORY" ]; then
-        exec "$@"
+    #INVENTORY DEFINED BY USER
+    if [ -f "$INVENTORY" ]; then
+        if [ -z "$CONNECTION_LOCAL" ]; then
+            exec "$@" -i $INVENTORY $PLAYBOOK
+        else
+            exec "$@" -i $INVENTORY $PLAYBOOK --connection=local
+        fi
+    #USE DEFAULT INVENTORY
     else
-        exec "$@" -i $INVENTORY
+        #ADD HOST TO INVENTORY FILE
+        if [ -n "$INVENTORY_HOST" ]; then
+            echo $INVENTORY_HOST >> /etc/ansible/hosts
+        fi
+        if [ -z "$CONNECTION_LOCAL" ]; then
+            exec "$@" -i $PLAYBOOK
+        else
+            exec "$@" -i $PLAYBOOK --connection=local
+        fi
     fi
-
 else
     exec "$@"
 fi
